694e0e8dc3007309b056b6585ed519b8f808bd58
guillaume.bouvignies <guillaume.bouvignies@kurtosistech.com>
2022-10-19 19:27:35 +0200

    Setup release workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@ set -euo pipefail
 script_dirpath="$(cd "$(dirname "${0}")" && pwd)"
 root_dirpath="$(dirname "${script_dirpath}")"
 
-IMAGE_NAME='kurtosistech/iproute2'
+IMAGE_ORG_AND_REPO='kurtosistech/iproute2'
 DOCKER_CONTEXT='iproute2_docker_context'
 SUPPORTED_PLATFORMS='linux/arm64/v8,linux/amd64'
 
@@ -45,6 +45,12 @@ esac
 # ========================================================================================================
 #                                           Main Code
 # ========================================================================================================
+if ! docker_tag="$(kudet get-docker-tag)"; then
+    echo "Error: Couldn't get the Docker image tag" >&2
+    exit 1
+fi
+image_name="${IMAGE_ORG_AND_REPO}:${docker_tag}"
+
 if "${do_build}"; then
     if ! [ -f "${root_dirpath}"/.dockerignore ]; then
         echo "Error: No .dockerignore file found in root; this is required so Docker caching works properly" >&2
@@ -52,12 +58,12 @@ if "${do_build}"; then
     fi
     push_flag=''
     if "${do_publish}"; then
-        echo "Info: Publish flag set to true, will publish images to DockerHub"
+        echo "Info: Publish flag set to true, building and publishing images '${image_name}' to DockerHub"
         push_flag='--push'
     else
-        echo "Info: Publish flag set to false, nothing will be published to DockerHub"
+        echo "Info: Publish flag set to false, docker images '${image_name}' will be built but nothing will be published to DockerHub"
     fi
     docker context create "${DOCKER_CONTEXT}"
     docker buildx create --use "${DOCKER_CONTEXT}"
-    docker buildx build ${push_flag} --platform "${SUPPORTED_PLATFORMS}" -t "${IMAGE_NAME}" -f "${root_dirpath}/Dockerfile" "${root_dirpath}"
+    docker buildx build ${push_flag} --platform "${SUPPORTED_PLATFORMS}" -t "${image_name}" -f "${root_dirpath}/Dockerfile" "${root_dirpath}"
 fi
